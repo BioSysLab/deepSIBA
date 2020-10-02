551579b6794bfeff7a73dc6f1f5f39575803effc
chris-ntua <chfoti@gmail.com>
2020-10-02 14:37:48 +0300

    new inference files for large scale

--- signaling_pathway_inference/PI_step_2.R
+++ signaling_pathway_inference/PI_step_2.R
@@ -12,7 +12,7 @@ all <- all %>% filter(!is.na(rdkit))
 # read pathway scores
 path_pa <- "pathway_inference_data/nes_compounds_all.rds"
 # path to the neighbor file from step 1
-neighbor_file <- "C:/Users/user/Documents/deepSIBA/results/inference_test_1/results_trainingset.csv"
+neighbor_file <- "C:/Users/user/Documents/deepSIBA/results/inference_test_jan_0/results_trainingset.csv"
 
 # read precalculated p value distributions for up and downregulated pathways
 dist_pb <- readRDS("pathway_inference_data/distributions_pb.rds")
@@ -116,7 +116,70 @@ infer_pathways <- function(cell_line, path_pa, neighbor_file, all, n_min_neighbo
   return(results)
 }
 
-test <- infer_pathways(cell_line = "MCF7",path_pa = path_pa,neighbor_file = neighbor_file,all = all,n_min_neighbors = 5,
+test <- infer_pathways(cell_line = "A375",path_pa = path_pa,neighbor_file = neighbor_file,all = all,n_min_neighbors = 5,
                        tb_pa = 10,dist_pb = dist_pb,dist_pt = dist_pt,fr_thresh = 0.65,p_val_thresh = 0.01,n_trials = 5000)
 
-test$`Nc1nc(F)nc2c1ncn2C1OC(CO)C(O)C1O`
+
+files <- list.files("C:/Users/user/Documents/deepSIBA/results/cannabis_pathways_a375/",all.files = T,recursive = T,full.names = T)
+cell <- "A375"
+names <- c("CBC","CBCA","CBCV","CBCVA","CBL","CBLA","CBD","CBDM","CBDA","CBD-C1","CBD-C4","CBDV","CBDVA","CBDP","6α-OH CBD",
+           "CBEA-B","CBE","CBG","CBGM","CBGA","CBGAM","CBGV","CBGVA","CBGO","CBGOA","CBND","CBN","CBNA","CBV",
+           "CBT","Δ8-THC","Δ8-THCA","THC","THC-C4","THCA-A","THCA-C4","THC-C1","THCA-C1-A","THCA-C1-B","THCV","THCVA",
+           "Δ9-THCP","CBF","CBR","CBT","DCBF","cis-THC")
+smiles <- c("CCCCCC1=CC(=C2C=CC(OC2=C1)(C)CCC=C(C)C)O","CCCCCC1=CC2=C(C=CC(O2)(C)CCC=C(C)C)C(=C1C(=O)O)O",
+           "CCCC1=CC(=C2C=CC(OC2=C1)(C)CCC=C(C)C)O","CCCC1=CC2=C(C=CC(O2)(C)CCC=C(C)C)C(=C1C(=O)O)O",
+           "CCCCCC1=CC(=C2C3C4C(C3(C)C)CCC4(OC2=C1)C)O","CCCCCC1=CC2=C(C3C4C(C3(C)C)CCC4(O2)C)C(=C1C(=O)O)O",
+           "CCCCCC1=CC(=C(C(=C1)O)C2C=C(CCC2C(=C)C)C)O","CCCCCC1=CC(=C(C(=C1)OC)C2C=C(CCC2C(=C)C)C)O",
+           "CCCCCC1=CC(=C(C(=C1C(=O)O)O)C2C=C(CCC2C(=C)C)C)O","CC1=CC(C(CC1)C(=C)C)C2=C(C=C(C=C2O)C)O",
+           "CC1=C[C@@H](C2=C(O)C=C(CCCC)C=C2O)[C@H](C(C)=C)CC1","CCCC1=CC(=C(C(=C1)O)C2C=C(CCC2C(=C)C)C)O",
+           "CCCC1=CC(=C(C(=C1C(=O)O)O)C2C=C(CCC2C(=C)C)C)O","OC(C=C(C=C1O)CCCCCCC)=C1[C@H]2[C@H](C(C)=C)CCC(C)=C2",
+           "OC(C=C(C=C1O)CCCCC)=C1[C@H]2[C@H](C(C)=C)C[C@H](O)C(C)=C2","CCCCCC1=CC(=C2C3C(CCC(C3OC2=C1C(=O)O)(C)O)C(=C)C)O",
+           "CCCCCC1=CC(=C2C3C(CCC(C3OC2=C1)(C)O)C(=C)C)O","CCCCCC1=CC(=C(C(=C1)O)CC=C(C)CCC=C(C)C)O",
+           "CC1=CC2=C(C(C)(C)OC3=C2C(OC)=CC(CCCCC)=C3)C=C1","CCCCCC1=CC(=C(C(=C1C(=O)O)O)CC=C(C)CCC=C(C)C)O",
+           "CCCCCC1=CC(=C(C(=C1C(=O)O)O)CC=C(C)CCC=C(C)C)OC","CCCC1=CC(=C(C(=C1)O)CC=C(C)CCC=C(C)C)O",
+           "CCCC1=CC(=C(C(=C1C(=O)O)O)CC=C(C)CCC=C(C)C)O","OC1=C(C/C=C(C)/CC/C=C(C)/C)C(O)=CC(C)=C1",
+           "OC1=C(C/C=C(C)/CC/C=C(C)/C)C(O)=C(C(O)=O)C(C)=C1","CCCCCC1=CC(=C(C(=C1)O)C2=C(C=CC(=C2)C)C(=C)C)O",
+           "CCCCCC1=CC(=C2C(=C1)OC(C3=C2C=C(C=C3)C)(C)C)O","CCCCCC1=CC2=C(C3=C(C=CC(=C3)C)C(O2)(C)C)C(=C1C(=O)O)O",
+           "CCCC1=CC(=C2C(=C1)OC(C3=C2C=C(C=C3)C)(C)C)O","CCCCCC1=CC(=C2C(=C1)OC(C3=C2C(C(CC3)(C)O)O)(C)C)O",
+           "CCCCCC1=CC(=C2C3CC(=CCC3C(OC2=C1)(C)C)C)O","CC1=CC[C@@]2([H])C(C)(C)OC(C=C(CCCCC)C(C(O)=O)=C3O)=C3[C@]2([H])C1",
+           "CCCCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O","CCCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O",
+           "CCCCCC1=CC2=C(C3C=C(CCC3C(O2)(C)C)C)C(=C1C(=O)O)O","CCCCC1=CC2=C([C@@H]3C=C(CC[C@H]3C(O2)(C)C)C)C(=C1)O",
+           "CC1=CC2C(CC1)C(OC3=CC(=CC(=C23)O)C)(C)C","CC1=CC2C(CC1)C(OC3=C2C(=C(C(=C3)C)C(=O)O)O)(C)C",
+           "CC1=CC2C(CC1)C(OC3=C(C(=CC(=C23)O)C)C(=O)O)(C)C","CCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O",
+           "CCCC1=CC2=C(C3C=C(CCC3C(O2)(C)C)C)C(=C1C(=O)O)O","CC1=C[C@]2([H])[C@@](C(C)(C)OC3=C2C(O)=CC(CCCCCCC)=C3)([H])CC1",
+           "CCCCCC1=CC(=C2C(=C1)OC3=C(C=CC(=C23)C(C)C)C)O","CCCCCC1=CC(=C2C3C(CCC(C3O)(C)O)C(OC2=C1)(C)C)O",
+           "CCCCCC1=CC2=C3C(OC(C)(C)C4C3CC(C)(CC4)O2)=C1","CCCCCC1=CC(=C2C(=C1)OC3=C(C=CC(=C23)C(=C)C)C)O",
+           "CCCCCC1=CC(=C2C3C=C(CCC3C(OC2=C1)(C)C)C)O")
+results_all <- data.frame(matrix("",nrow=0,ncol=4))
+colnames(results_all) <- c("name","smile","downregulated","upregulated")
+for (i in 1:length(files)) {
+  file <- read.csv(files[i])
+  
+  if (nrow(file)<5) {
+    df_results <- data.frame(matrix("",nrow=1,ncol=4))
+    colnames(df_results) <- c("name","smile","downregulated","upregulated")
+    df_results$name <- names[i]
+    df_results$smile <- smiles[i]
+    df_results$downregulated <- ""
+    df_results$upregulated <- ""
+    
+  }else {
+    
+    results <- infer_pathways(cell_line = cell,path_pa = path_pa,neighbor_file = files[i],all = all,n_min_neighbors = 5,
+                                   tb_pa = 10,dist_pb = dist_pb,dist_pt = dist_pt,fr_thresh = 0.65,p_val_thresh = 0.01,n_trials = 5000)
+    df_results <- data.frame(matrix("",nrow=10,ncol=4))
+    colnames(df_results) <- c("name","smile","downregulated","upregulated")
+    df_results$name <- names[i]
+    df_results$smile <- smiles[i]
+    len_down <- length(results[[1]]$Downregulated)
+    len_up <- length(results[[1]]$Upregulated)
+    df_results$downregulated <- as.character(df_results$downregulated)
+    df_results$upregulated <- as.character(df_results$upregulated)
+    df_results$downregulated[1:len_down] <- as.character(results[[1]]$Downregulated)
+    df_results$upregulated[1:len_up] <- results[[1]]$Upregulated
+  }
+  results_all <- bind_rows(results_all,df_results)
+}
+write.csv(results_all,"C:/Users/user/Documents/deepSIBA/results/cannabis_pathways_a375/a375_cannabis_pathways.csv",row.names = F)
+
+

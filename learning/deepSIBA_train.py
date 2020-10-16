23d60c593b9a91ecad2b9e275efe52a17ed935b6
Nikolaos Meimetis <nmeimetis97@gmail.com>
2020-10-16 15:15:25 +0300

    Update deepSIBA_train.py
    corrected for the case of alldata!

--- learning/deepSIBA_train.py
+++ learning/deepSIBA_train.py
@@ -61,14 +61,6 @@ def siba_trainer(train_params, model_params):
                 Y_cold = Y_cold/2
         elif train_params["split"] == "alldata":
             df = pd.read_csv("data/" + train_params["cell_line"] + "/" + "alldata/" + "alldata_" + train_params["cell_line"] + ".csv",index_col=0).reset_index(drop=True)
-            df_cold = pd.read_csv("data/" + train_params["cell_line"] + "/" + "train_test_split/" + "test.csv",index_col=0).reset_index(drop=True)
-            smiles_cold = list(set(list(df_cold['rdkit.x'])+list(df_cold['rdkit.y'])))
-            X_atoms_cold, X_bonds_cold, X_edges_cold = tensorise_smiles(smiles_cold,  model_params["max_degree"], model_params["max_atoms"])
-            if train_params["test_value_norm"]:
-                Y_cold = df_cold.value
-            else:
-                Y_cold = df_cold.value
-                Y_cold = Y_cold/2
         elif train_params["split"] == "5_fold_cv_split":
             df = pd.read_csv("data/" + train_params["cell_line"] + "/" + "5_fold_cv_split/" + "fold_%s/train_%s.csv" %(i+1,i+1),index_col=0).reset_index(drop=True)
             df_cold = pd.read_csv("data/" + train_params["cell_line"] + "/" + "5_fold_cv_split/" + "fold_%s/val_%s.csv" %(i+1,i+1),index_col=0).reset_index(drop=True)
@@ -79,10 +71,11 @@ def siba_trainer(train_params, model_params):
             else:
                 Y_cold = df_cold.value
                 Y_cold = Y_cold/2
-        Path(train_params["output_dir"] + "/" + "fold_%s/cold/mu"%i).mkdir(parents=True, exist_ok=True)
-        Path(train_params["output_dir"] + "/" + "fold_%s/cold/sigma"%i).mkdir(parents=True, exist_ok=True)
+        if train_params["split"] != "alldata":
+            Path(train_params["output_dir"] + "/" + "fold_%s/cold/mu"%i).mkdir(parents=True, exist_ok=True)
+            Path(train_params["output_dir"] + "/" + "fold_%s/cold/sigma"%i).mkdir(parents=True, exist_ok=True)
+            Path(train_params["output_dir"] + "/" + "fold_%s/performance"%i).mkdir(parents=True, exist_ok=True)
         Path(train_params["output_dir"] + "/" + "fold_%s/models"%i).mkdir(parents=True, exist_ok=True)
-        Path(train_params["output_dir"] + "/" + "fold_%s/performance"%i).mkdir(parents=True, exist_ok=True)
         cold_preds_mus = []
         cold_preds_sigmas = []
         n = train_params["nmodel_start"]
@@ -114,34 +107,36 @@ def siba_trainer(train_params, model_params):
                             callbacks= [term, rlr])
             if history.history["r_square"][len(history.history["r_square"])-1] >= 0.7:
                 deepsiba.save_weights(train_params["output_dir"] + "/" + "fold_%s/models/"%i + "model_%s.h5"%n)
-                gaussian = keras.Model(deepsiba.inputs, deepsiba.get_layer('main_output').output)
-                pr_steps=ceil(len(df_cold)/train_params["predict_batch_size"])
-                PredGen=preds_generator(train_params["predict_batch_size"],df_cold,smiles_cold,X_atoms_cold, X_bonds_cold, X_edges_cold,gaussian)
-                y_pred1=[]
-                y_pred2=[]
-                for g in range(pr_steps):
-                    cold_pred=list(next(PredGen))
-                    y_pred1=y_pred1+list(cold_pred[0])
-                    y_pred2=y_pred2+list(cold_pred[1])
-                y_pred1=np.array(y_pred1)
-                y_pred2=np.array(y_pred2)
-                if (len(y_pred1[np.where(y_pred1 <= train_params["prec_threshold"])])>0):
-                    get = model_evaluate(y_pred1,Y_cold,train_params["prec_threshold"],df_cold)
-                    get.to_csv(train_params["output_dir"] + "/" + "fold_%s/performance/"%i + "model_%s.csv"%n)
-                cold_preds_mus.append(y_pred1)
-                np.save(train_params["output_dir"] + "/" + "fold_%s/cold/mu/"%i + "cold_mu_%s.npy"%n, y_pred1)
-                cold_preds_sigmas.append(y_pred2)
-                np.save(train_params["output_dir"] + "/" + "fold_%s/cold/sigma/"%i + "cold_sigma_%s.npy"%n, y_pred2)
+                if train_params["split"] != "alldata":
+                    gaussian = keras.Model(deepsiba.inputs, deepsiba.get_layer('main_output').output)
+                    pr_steps=ceil(len(df_cold)/train_params["predict_batch_size"])
+                    PredGen=preds_generator(train_params["predict_batch_size"],df_cold,smiles_cold,X_atoms_cold, X_bonds_cold, X_edges_cold,gaussian)
+                    y_pred1=[]
+                    y_pred2=[]
+                    for g in range(pr_steps):
+                        cold_pred=list(next(PredGen))
+                        y_pred1=y_pred1+list(cold_pred[0])
+                        y_pred2=y_pred2+list(cold_pred[1])
+                    y_pred1=np.array(y_pred1)
+                    y_pred2=np.array(y_pred2)
+                    if (len(y_pred1[np.where(y_pred1 <= train_params["prec_threshold"])])>0):
+                        get = model_evaluate(y_pred1,Y_cold,train_params["prec_threshold"],df_cold)
+                        get.to_csv(train_params["output_dir"] + "/" + "fold_%s/performance/"%i + "model_%s.csv"%n)
+                    cold_preds_mus.append(y_pred1)
+                    np.save(train_params["output_dir"] + "/" + "fold_%s/cold/mu/"%i + "cold_mu_%s.npy"%n, y_pred1)
+                    cold_preds_sigmas.append(y_pred2)
+                    np.save(train_params["output_dir"] + "/" + "fold_%s/cold/sigma/"%i + "cold_sigma_%s.npy"%n, y_pred2)
                 n = n + 1
-        mu_star=np.mean(cold_preds_mus,axis=0)
-        sigma_star = np.sqrt(np.mean(cold_preds_sigmas + np.square(cold_preds_mus), axis = 0) - np.square(mu_star))
-        cv_star = sigma_star/mu_star
-        if (len(mu_star[np.where(mu_star <= train_params["prec_threshold"])])>0):
-            get_fold = model_evaluate(mu_star,Y_cold,train_params["prec_threshold"],df_cold)
-            get_fold.to_csv(train_params["output_dir"] + "/" + "fold_%s/ensemble_performance.csv"%i)
-            get_all.append(get_fold)
-        df_cold['mu'] = mu_star
-        df_cold['cv'] = cv_star
-        df_cold.to_csv(train_params["output_dir"] + "/" + "fold_%s/ensemble_preds_dataframe.csv"%i)
+        if train_params["split"] != "alldata":
+            mu_star=np.mean(cold_preds_mus,axis=0)
+            sigma_star = np.sqrt(np.mean(cold_preds_sigmas + np.square(cold_preds_mus), axis = 0) - np.square(mu_star))
+            cv_star = sigma_star/mu_star
+            if (len(mu_star[np.where(mu_star <= train_params["prec_threshold"])])>0):
+                get_fold = model_evaluate(mu_star,Y_cold,train_params["prec_threshold"],df_cold)
+                get_fold.to_csv(train_params["output_dir"] + "/" + "fold_%s/ensemble_performance.csv"%i)
+                get_all.append(get_fold)
+            df_cold['mu'] = mu_star
+            df_cold['cv'] = cv_star
+            df_cold.to_csv(train_params["output_dir"] + "/" + "fold_%s/ensemble_preds_dataframe.csv"%i)
     return(get_all)
 
